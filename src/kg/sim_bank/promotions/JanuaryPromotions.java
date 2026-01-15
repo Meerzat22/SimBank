87f8b851f294fda44fc5acbe2a29305ae02610b1
taalaibekovameerzat <taalajbekovameerzat@gmail.com>
2026-01-15 09:36:19 +0600

    January promo 123.
    january promotion 1
    
    january promotion 2
    
    january promotion 3
    
    january promotion 4
    
    january promotion 5
    
    january promotion 6

---
+++ src/kg/sim_bank/promotions/JanuaryPromotions.java
@@ -0,0 +1,15 @@
+package kg.sim_bank.promotions;
+
+public class JanuaryPromotions {
+    public static void main(String[] args) {
+
+        System.out.println("January p");
+        System.out.println("January 123");
+        System.out.println("January 123 4");
+        System.out.println("January 123 5");
+        System.out.println("January 123 6");
+        System.out.println("January 123 7");
+        System.out.println("January 123 8888");
+        System.out.println("January 123 8888   999 10");
+    }
+}
